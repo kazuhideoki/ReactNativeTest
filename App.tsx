b1d42ebc296c8f0e0bd339867d88374fdf477be9
Kazuhide Oki <cedar123pc@gmail.com>
2021-03-13 21:40:16 +0900

    firestoreからデータ取得表示完了

--- App.tsx
+++ App.tsx
@@ -1,11 +1,50 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+import * as firebase from "firebase";
+
+import "firebase/firestore";
+import serviceAccount from "./serviceAccountKey.json";
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: serviceAccount.private_key,
+  projectId: serviceAccount.project_id,
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+console.log(db);
 
 export default function App() {
+  const [users, setUsers] = React.useState([]);
+
+  const logUsers = () => console.log(users);
+
+  React.useEffect(() => {
+    db.collection("user")
+      .get()
+      .then((querySnapshot) => {
+        const docs = querySnapshot.docs;
+        console.log("useEffect");
+        let list = [];
+        docs.forEach((doc) => list.push(doc.data()));
+
+        setUsers(list);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Text>できたよ</Text>
+      <Button title="セット" onPress={() => logUsers()} />
+      {/* <Button title="セット" onPress={() => setData()} /> */}
+      {users.map((user) => (
+        <Text>{user.name}</Text>
+      ))}
     </View>
   );
 }
